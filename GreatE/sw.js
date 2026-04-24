e010d6697fc35480e6cf57646ffe5fceb9d46f3c
DssOrit <oritqumran@gmail.com>
2026-04-24 22:10:59 +0100

    Add files via upload
    Signed-off-by: DssOrit <oritqumran@gmail.com>

---
+++ GreatE/sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'great-eraser-v1';
+const PRECACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network-first for data files, cache-first for shell
+  const url = new URL(e.request.url);
+  if (url.pathname.includes('/data/')) {
+    e.respondWith(
+      fetch(e.request)
+        .then(r => {
+          const clone = r.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return r;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request))
+    );
+  }
+});
